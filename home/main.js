d26ac13b21a27af9c7c75e8b652190be93ed3b13
ImTonyS <tony.villaverde@hotmail.com>
2023-11-20 02:05:09 -0700

    UPDATE 20/11

---
+++ home/main.js
@@ -0,0 +1,52 @@
+const menu = document.querySelector('.nav_menu');
+const menuList = document.querySelector('.nav_list');
+const links = document.querySelectorAll('.nav_link');
+const wrapper = document.querySelector('.wrapper'),
+    wrapperClose = document.querySelector('.wrapper_close')
+    signupHeader = document.querySelector('.signup header'),
+    loginHeader = document.querySelector('.login header'),
+    signupBtn = document.querySelector('#signupbtn');
+
+    
+const hero = document.querySelector('.hero'),
+    herocta = document.querySelector('#hero_cta');
+
+// BOTON .hero_cta LLAMA POP SIGN UP
+herocta.addEventListener('click', () => {
+    wrapper.classList.add("index");
+})
+// BOTON .wrapper_close LO DESAPARECE
+wrapperClose.addEventListener('click', () => {
+    wrapper.classList.remove("index");
+})
+
+signupBtn.addEventListener('click', () => {
+    wrapper.classList.add("index");
+    menuList.classList.remove("nav_list--show")
+})
+
+
+
+// ANIMACION DEL LOGIN Y SIGNUP
+loginHeader.addEventListener('click', () => {
+    wrapper.classList.add("active");
+})
+signupHeader.addEventListener('click', () => {
+    wrapper.classList.remove("active");
+})
+
+// CLICK EN MENU SANDW LLAMA LA LISTA DE OPCIONES
+menu.addEventListener('click', function(){
+
+    menuList.classList.toggle('nav_list--show');
+
+})
+
+links.forEach(function(link){
+
+    link.addEventListener('click', function(){
+
+        menuList.classList.remove('nav_list--show');
+
+    });
+}); 
